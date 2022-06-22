9e6bebb05f13643d7f43d266c54fce9064565d9d
Chisom <83100384+titanwarlord007@users.noreply.github.com>
2022-06-22 14:59:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-const c = console.log.bind(document)
-
-
-
 const calculate =()=>{
     const operator = prompt('what operation do you want to perform: +, -, *, / ');
 const firstNo = Number( prompt('first number'));
 const secondNo=Number(prompt('second number'));
-let sum=0
-operator=== '+'? sum=alert(`your answer is ${firstNo+secondNo}`):
-operator=== '-'? sum=alert(`your answer is ${firstNo-secondNo}`):
-operator=== '*'? sum=alert(`your answer is ${firstNo*secondNo}`):
-operator=== '/'? sum=alert(`your answer is ${firstNo/secondNo}`):
+let calculation=0
+operator=== '+'?calculation=alert(`your answer is ${firstNo+secondNo}`):
+operator=== '-'? calculation=alert(`your answer is ${firstNo-secondNo}`):
+operator=== '*'? calculation=alert(`your answer is ${firstNo*secondNo}`):
+operator=== '/'? calculation=alert(`your answer is ${firstNo/secondNo}`):
 alert('not a specified operator')
 
 
